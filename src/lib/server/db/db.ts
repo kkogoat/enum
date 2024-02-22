c341aeb536f29f6f9e23693fa0ca48cf95f75bdd
dhv <49603207+kkogoat@users.noreply.github.com>
2024-02-22 12:28:14 -0800

    server log util

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -2,6 +2,7 @@ import { DB_NAME, DB_USER, DB_PWD, DB_HOST, DB_PORT } from '$env/static/private'
 import { Sequelize } from "sequelize-typescript";
 import User from './models/user.js';
 import Media from "./models/media.js";
+import { log } from '../util/loggerUtil.js';
 
 // DB CONFIG
 const sequelize = new Sequelize ({
@@ -11,23 +12,26 @@ const sequelize = new Sequelize ({
     password: DB_PWD,
     host: DB_HOST,
     port: Number(DB_PORT),
+    logging: (msg) => {
+        log("db", msg)
+    },
     models: [User, Media],
 });
 
 // DB CONNECTION
 try {
     await sequelize.authenticate();
-    console.log("db", 'connection established');
+    log("db", 'connection established');
 } catch(err) {
-    console.log("db", 'connection unable to establish');
+    log("db", 'connection unable to establish');
 }
 
 // DB SYNC
 try {
     await sequelize.sync();
-    console.log("db", 'data models synced');
+    log("db", 'data models synced');
 } catch(err) {
-    console.log("db", 'data models unable to sync');
+    log("db", 'data models unable to sync');
 }
 
 export default sequelize;
---
+++ src/lib/server/util/loggerUtil.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+
+// LOG CREATION
+let today: Date = new Date();
+let todayString: string = String(today.getMonth()+1).padStart(2, '0') + '-'
+                        + String(today.getDate()).padStart(2, '0') + '-'
+                        + String(today.getFullYear());
+
+const serverFS = fs.createWriteStream(
+    `./logs/${todayString}_server.log`, 
+    {'flags': 'w'} // @TODO CHANGE TO APPEND WHEN BACKEND IN FINISHED STATE
+);
+
+// LOGGER
+export const log = (system: string, message: string) => {
+
+    // MSG BUILING
+    let currTime: Date = new Date();
+    let timestamp: string = String(currTime.getHours()).padStart(2, '0') + ":"
+                            + String(currTime.getMinutes()).padStart(2, '0') + ":"
+                            + String(currTime.getSeconds()).padStart(2, '0');
+
+    let msg: string = "[" + timestamp + "] ";
+    msg += "[" + system + "] ";
+    msg += message + "\n";
+
+    // WRITE
+    serverFS.write(msg);
+}
