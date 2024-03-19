d80c62251c50a98da72aae3fef8a6309c47c29bc
dhv <49603207+kkogoat@users.noreply.github.com>
2024-03-19 14:10:00 -0700

    hover interaction on title

--- src/lib/mediaItem.svelte
+++ src/lib/mediaItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     export let item: any;
     export let pos: any;
+    let buttonRef: HTMLButtonElement;
     
     // DESTRUCTURE ITEM
     let id: string = item["id"];
@@ -19,7 +20,6 @@
         display: grid;
         grid-template-columns: minmax(200px, 500px) 100px 100px 100px;
         border-bottom: 1px solid var(--foreground-color);
-        align-items: center;
     }
 
     .pos-1 {
@@ -30,8 +30,8 @@
     }
 
     .media-item {
-        padding-left: 8px;
-        margin: 0px;
+        height: 100%;
+        padding: 1px 0px 0px 8px;
     }
     .media-item-border {
         border-right: 1px solid var(--foreground-color);
@@ -41,11 +41,28 @@
         justify-self: center;
     }
 
+    .edit-button {
+        float: right;
+        margin: 5px 7px 0px 0px;
+        display: none;
+    }
+    .title:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    .title:hover > .edit-button {
+        display: block;
+        background-color: var(--accent-color-hover);
+    }
+    .title:active > .edit-button {
+        background-color: var(--accent-color-active);
+    }
 </style>
 
 <div class="media-item-container {pos % 2 == 0 ? "pos-1" : "pos-2"}">
-    <div class="media-item media-item-border">
+    <div class="media-item media-item-border title" on:click|self={() => buttonRef.click()} aria-hidden="true">
         {item["title"]}
+        <button class="edit-button" bind:this={buttonRef} on:click={() => console.log("clicked")}>open</button>
     </div>
     <div class="media-item media-item-border">
         {item["rating"] ? item["rating"] : "-"}
