13475cc35aceee17d0059be0f96445417b694220
dhv <49603207+kkogoat@users.noreply.github.com>
2024-03-21 21:23:35 -0700

    meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
     import '../app.css';
 </script>
 
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/usagi.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>enum.usagi</title>
+</head>
+
 <slot />
