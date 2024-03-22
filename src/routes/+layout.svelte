6c280e0e0c6501713087dec58b81c8030a1495cb
dhv <49603207+kkogoat@users.noreply.github.com>
2024-03-21 21:29:59 -0700

    favicon bug fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,9 @@
     import '../app.css';
 </script>
 
-<head>
-    <meta charset="UTF-8" />
+<svelte:head>
     <link rel="icon" type="image/svg+xml" href="/usagi.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>enum.usagi</title>
-</head>
+</svelte:head>
 
 <slot />
