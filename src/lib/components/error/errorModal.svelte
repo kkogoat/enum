16041e1d4498816c33af35414aeebd13f9570370
dhv <49603207+kkogoat@users.noreply.github.com>
2024-03-28 14:28:13 -0700

    extended error modal for ok messages

--- src/lib/components/error/errorModal.svelte
+++ src/lib/components/error/errorModal.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     let errorRef: HTMLDialogElement;
     export let errorMessage: string;
+    let ok: boolean = false;
 
-    export function showModal() {
+    export function showModal(mode: string = "") {
+        if(mode == "ok") {
+            ok = true;
+        }
         errorRef.showModal();
     }
 </script>
 
 <style>
+    .ok-dialog {
+        color: var(--text-color-ok);
+    }
     .error-dialog {
         color: var(--text-color-error);
     }
-    .error-dialog button {
+    .error-dialog-button {
         padding: 8px 12px 8px 12px;
         margin-top: 15px;
         margin-left: 36%;
@@ -20,7 +27,7 @@
 </style>
 
 <!-- ERROR -->
-<dialog class="error-dialog" bind:this={errorRef}>
+<dialog class="{ok ? "ok-dialog" : "error-dialog"}" bind:this={errorRef}>
     <div>{errorMessage}</div>
-    <button on:click={() => errorRef.close()}>はい</button>
+    <button class="error-dialog-button"on:click={() => errorRef.close()}>はい</button>
 </dialog>
