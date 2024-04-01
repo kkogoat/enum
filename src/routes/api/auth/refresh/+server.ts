fdb96675aea0ea9c208fe1e06bce6f60a79dd3d5
dhv <49603207+kkogoat@users.noreply.github.com>
2024-04-01 16:16:10 -0700

    consolidated refresh & auto-login into one
    + rotating refresh tokens

--- src/routes/api/auth/refresh/+server.ts
+++ src/routes/api/auth/refresh/+server.ts
@@ -1,34 +1,50 @@
-import { ACCESS_TOKEN_EXPIRY, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '$env/static/private';
-import User from '$lib/server/db/models/user.js';
-import { log } from '$lib/server/util/loggerUtil.js';
+import { REFRESH_TOKEN_SECRET } from "$env/static/private";
+import { generateTokens } from "$lib/server/util/authUtil.js";
+import { log } from "$lib/server/util/loggerUtil.js";
+import User from "$lib/server/db/models/user.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken';
+
+function jwtErrorHandling(err: any) {
+    switch(err.name) {
+        case "NotBeforeError":
+        case "TokenExpiredError": // Expired Token
+            return new Response(JSON.stringify(err.message), {status: 401});
+        case "JsonWebTokenError": // Unprocessable Refresh
+            return new Response(JSON.stringify("Bad Refresh Token"), {status: 422});
+    }
+}
 
 /** @type {import('./$types').RequestHandler} */
 export const GET = async ({ cookies }) => {
-    
-    // CHECK IF COOKIE EXISTS
+
+    // CHECK IF REFRESH EXISTS
     const refresh = cookies.get("session");
-    if(!refresh) return new Response(JSON.stringify("No Cookie"), {status: 400});
+    if(!refresh) return new Response(JSON.stringify("No Required Cookie"), {status: 400});
 
-    // VERIFY REFRESH
-    const result = await jwt.verify(refresh, REFRESH_TOKEN_SECRET, async (err, user) => {
-        // IF EXPIRED -> FORBIDDEN
-        if(err) return new Response(JSON.stringify(err.message), {status: 403});
+    // IF REFRESH EXISTS, VERIFY
+    const refresh_result = await jwt.verify(refresh, REFRESH_TOKEN_SECRET, async (err, user) => {
+        // ERRORS?
+        if(err) return jwtErrorHandling(err);
 
-        // COMPARE STORED REFRESH_TOKEN & COOKIE
+        // IF NO ERRORS, CHECK IF SESSION EXISTS
+        // @TODO: MULTIPLE SESSIONS
         const username: string = (user as JwtPayload).username;
         const instance = await User.findOne({where: {username: username}});
         if(!instance || !await bcrypt.compare(refresh, instance.refresh_token)) {
-            log("auth", `bad refresh for ${username}`);
-            return new Response(JSON.stringify("Bad Refresh"), {status: 403});
+            log(`auth`, `rejected refresh for user: ${username}`);
+            return new Response(JSON.stringify("Unauthorized Refresh Token"), {status: 401});
         }
-        
-        // SIGN NEW ACCESS_TOKEN
-        log("auth", `refresh user: ${instance.username}`);
-        const access_token = jwt.sign({username: username}, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRY });
-        return new Response(JSON.stringify({username: username, access_token: access_token}), {status: 200});
+
+        // SIGN NEW TOKENS, ROTATE REFRESH TOKEN
+        const tokens = generateTokens({username: username});
+        instance.update({refresh_token: await bcrypt.hash(tokens.refresh_token, 10)});
+
+        // UPDATE EXISTING COOKIE & RESPONSE
+        cookies.set("session", tokens.refresh_token, {path: '/', httpOnly: true, sameSite: 'strict', secure: true});
+        log(`auth`, `accepted refresh for user: ${username}`);
+        return new Response(JSON.stringify({username: username, access_token: tokens.access_token}), {status: 200});
     })
 
-    return result as unknown as Response;
+    return refresh_result as any as Response;
 }
