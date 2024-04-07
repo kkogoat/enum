fb80074935fd73ddd2046ee9b94bd3a1dd799143
dhv <49603207+kkogoat@users.noreply.github.com>
2024-04-07 16:31:33 -0700

    redirect from signup page if already logged in

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,17 +2,22 @@
     import { PUBLIC_ALLOW_NEW_ACC } from "$env/static/public";
 	import Signup from "$lib/components/signup/signup.svelte";
 	import ThemeToggle from "$lib/components/theme/themeToggle.svelte";
+	import { authContext } from "$lib/context/authContext";
 	import { onMount } from "svelte";
 
     // REDIRECT ON FALSE
-    onMount(() => {
-        if(PUBLIC_ALLOW_NEW_ACC == "false") {
+    let loading = true;
+    onMount(async () => {
+        await authContext.autoLogin();
+        if(PUBLIC_ALLOW_NEW_ACC == "false" || $authContext) {
             window.location.replace("/");
+        } else {
+            loading = false;
         }
     })
 </script>
 
-{#if PUBLIC_ALLOW_NEW_ACC == "true"}
+{#if PUBLIC_ALLOW_NEW_ACC == "true" && !loading}
     <ThemeToggle />
     <Signup />
 {/if}
